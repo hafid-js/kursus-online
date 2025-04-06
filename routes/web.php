ac4f7b5faaeb587903fb1ed157f07121d7c5d9d0
hafidjs <afidzpratama@gmail.com>
2025-04-06 20:00:08 +0700

    multi auth, separating student and instructor role

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\InstructorDashboardController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Student Routes
+Route::group(['middleware' => ['auth:web', 'verified','check_role:student'], 'prefix' => 'student', 'as' => 'student.'], function() {
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Instructor Routes
+Route::group(['middleware' => ['auth:web', 'verified','check_role:instructor'], 'prefix' => 'instructor', 'as' => 'instructor.'], function() {
+    Route::get('/dashboard', [InstructorDashboardController::class, 'index'])->name('dashboard');
 });
 
 Route::get('/admin/dashboard', function () {
