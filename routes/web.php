ef90b58b4e8c732e98c107606eccb2ab3f74cdbd
hafidjs <afidzpratama@gmail.com>
2025-04-09 05:44:05 +0700

    implementation admin bootstrap template

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,6 @@ Route::group(['middleware' => ['auth:web', 'verified','check_role:instructor'], 
     Route::get('/dashboard', [InstructorDashboardController::class, 'index'])->name('dashboard');
 });
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
 
