a35e6afd61b54e5febbe339f768c37ae0a3061c6
hafidjs <afidzpratama@gmail.com>
2025-06-20 15:42:34 +0700

    add feature contact form handling (fix)

--- app/Http/Controllers/Frontend/FrontendContactController.php
+++ app/Http/Controllers/Frontend/FrontendContactController.php
@@ -3,24 +3,49 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactMail;
 use App\Models\Contact;
 use App\Models\ContactSetting;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\Mail;
 
 class FrontendContactController extends Controller
 {
-    function index() {
+    function index()
+    {
         $contactCards = Contact::where('status', 1)->get();
         $contactSetting = ContactSetting::first();
-        return view('frontend.pages.contact', compact('contactCards','contactSetting'));
+        return view('frontend.pages.contact', compact('contactCards', 'contactSetting'));
     }
 
-    function sendMail(Request $request) {
+    function sendMail(Request $request)
+    {
         $request->validate([
-            'name' => ['required','string','max:255'],
-            'email' => ['required','string','email','max:255'],
-            'subject' => ['required','string','max:255'],
-            'message' => ['required','string','max:255'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'subject' => ['required', 'string', 'max:255'],
+            'message' => ['required', 'string', 'max:255'],
         ]);
+
+        if (config('mail_queue.is_queue')) {
+            Mail::to(config('settings.receiver_email'))->queue(new ContactMail(
+                $request->name,
+                $request->email,
+                $request->subject,
+                $request->message
+            ));
+        } else {
+            Mail::to(config('settings.receiver_email'))->send(new ContactMail(
+                $request->name,
+                $request->email,
+                $request->subject,
+                $request->message
+            ));
+        }
+
+        notyf()->success('Sent Successfully!');
+
+        return redirect()->back();
     }
 }
