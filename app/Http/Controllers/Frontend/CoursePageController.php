46b646f134bbf3294491bf7f1eca9e650a98d2f8
hafidjs <afidzpratama@gmail.com>
2025-06-21 13:23:38 +0700

    add feature filtering search courses

--- app/Http/Controllers/Frontend/CoursePageController.php
+++ app/Http/Controllers/Frontend/CoursePageController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\CourseCategory;
+use App\Models\CourseLanguage;
+use App\Models\CourseLevel;
 use Illuminate\Http\Request;
 
 class CoursePageController extends Controller
 {
-    function index() {
-        $courses = Course::where('is_approved','approved')->paginate(12);
-        return view('frontend.pages.course-page', compact('courses'));
+    function index(Request $request) {
+        $courses = Course::where('is_approved','approved')->where('status','active')->when($request->has('search') && $request->filled('search'), function($query) use ($request) {
+            $query->where('title','like','%'.$request->search.'%')->orWhere('description','like','%'.$request->search.'%');
+        })->paginate(12);
+        $categories = CourseCategory::where('status','1')->whereNull('parent_id')->get();
+        $levels = CourseLevel::all();
+        $languages = CourseLanguage::all();
+        return view('frontend.pages.course-page', compact('courses', 'categories','levels','languages'));
     }
 
     function show(string $slug) {
